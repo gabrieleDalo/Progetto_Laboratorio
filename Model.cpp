b2280cd8e66f1fe8a5ac54369162df943848e615
gabrieledalo <gabriele.dalo@stud.unifi.it>
2020-07-19 17:29:15 +0200

    Fixed Model header and cpp file.

--- Model.cpp
+++ Model.cpp
@@ -24,6 +24,85 @@ void Model::notify(int x,int y,string value) {
         observer->update(x,y,value);
 }
 
+void Model::onAddFormula(int row,int column,int currentRow,int currentColumn){
+    if(values.find(make_pair(currentRow,currentColumn)) != values.end() && values.find(make_pair(row,column)) != values.end()) {
+        values.find(make_pair(currentRow, currentColumn))->second.addSubject(values.find(make_pair(row, column))->second.getCell());
+        values.find(make_pair(row, column))->second.addObserver(values.find(make_pair(currentRow, currentColumn))->second.getCell());
+    }
+}
+
+pair<bool,list<pair<int,int>>> Model::onChangeCellFormula(int row,int column){
+    return values.find(make_pair(row,column))->second.notifyCell(row,column);
+}
+
+bool Model::findValue(int x,int y){
+    if(x < 0 || x > width || y < 0 || y > height){
+        throw out_of_range("Out of range values");
+    }else{
+        if(values.find(make_pair(x,y)) != values.end())
+            return true;
+        else
+            return false;
+    }
+}
+
+float Model::getValue(int x,int y) const {
+    if(x < 0 || x > width || y < 0 || y > height){
+        throw out_of_range("Out of range values");
+    }else{
+        if(values.find(make_pair(x,y)) != values.end())
+            return values.find(make_pair(x,y))->second.getValue();
+        else
+            return 0;
+    }
+}
+
+string Model::getFormula(int x,int y) const {
+    if(x < 0 || x > width || y < 0 || y > height){
+        throw out_of_range("Out of range values");
+    }else{
+        if(values.find(make_pair(x,y)) != values.end())
+            return values.find(make_pair(x,y))->second.getFormula();
+        else
+            return "";
+    }
+}
+
+void Model::setValue(int x,int y,int row,int column,float value,string formula,bool modified) {
+    if(x < 0 || x > width || y < 0 || y > height || row < 0 || row > width || column < 0 || column > width-1){
+        throw out_of_range("Out of range values");
+    }else{
+
+        if(modified){
+            values.find(make_pair(row,column))->second.removeObservers();
+            values.find(make_pair(row,column))->second.removeSubjects();
+        }
+
+        values[make_pair(row,column)].setRow(row);
+        values[make_pair(row,column)].setColumn(column);
+        values[make_pair(row,column)].setValue(value);
+        values[make_pair(row,column)].setFormula(formula);
+        notify(x,y,value);
+    }
+}
+
+void Model::setValue(int x,int y,int row,int column,string value,string formula,bool modified) {
+    if(x < 0 || x > width || y < 0 || y > height || row < 0 || row > width || column < 0 || column > width-1){
+        throw out_of_range("Out of range values");
+    }else{
+        if(modified){
+            values.find(make_pair(row,column))->second.removeObservers();
+            values.find(make_pair(row,column))->second.removeSubjects();
+        }
+
+        values[make_pair(row,column)].setRow(row);
+        values[make_pair(row,column)].setColumn(column);
+        values[make_pair(row,column)].setValue(0);
+        values[make_pair(row,column)].setFormula(formula);
+        notify(x,y,value);
+    }
+}
+
 int Model::getWidth() const {
     return width;
 }
--- Model.h
+++ Model.h
@@ -31,84 +31,19 @@ public:
 
     void notify(int x,int y,string value) override;
 
-    void onAddFormula(int row,int column,int currentRow,int currentColumn){
-        if(values.find(make_pair(currentRow,currentColumn)) != values.end() && values.find(make_pair(row,column)) != values.end()) {
-            values.find(make_pair(currentRow, currentColumn))->second.addSubject(values.find(make_pair(row, column))->second.getCell());
-            values.find(make_pair(row, column))->second.addObserver(values.find(make_pair(currentRow, currentColumn))->second.getCell());
-        }
-    }
+    void onAddFormula(int row,int column,int currentRow,int currentColumn);
 
-    pair<bool,list<pair<int,int>>> onChangeCellFormula(int row,int column){
-        return values.find(make_pair(row,column))->second.notifyCell(row,column);
-    }
+    pair<bool,list<pair<int,int>>> onChangeCellFormula(int row,int column);
 
-    bool findValue(int x,int y){
-        if(x < 0 || x > width || y < 0 || y > height){
-            throw out_of_range("Out of range values");
-        }else{
-            if(values.find(make_pair(x,y)) != values.end())
-                return true;
-            else
-                return false;
-        }
-    }
+    bool findValue(int x,int y);
 
-    float getValue(int x,int y) const {
-        if(x < 0 || x > width || y < 0 || y > height){
-            throw out_of_range("Out of range values");
-        }else{
-            if(values.find(make_pair(x,y)) != values.end())
-                return values.find(make_pair(x,y))->second.getValue();
-            else
-                return 0;
-        }
-    }
+    float getValue(int x,int y) const;
 
-    string getFormula(int x,int y) const {
-        if(x < 0 || x > width || y < 0 || y > height){
-            throw out_of_range("Out of range values");
-        }else{
-            if(values.find(make_pair(x,y)) != values.end())
-                return values.find(make_pair(x,y))->second.getFormula();
-            else
-                return "";
-        }
-    }
+    string getFormula(int x,int y) const;
 
-    void setValue(int x,int y,int row,int column,float value,string formula,bool modified) {
-        if(x < 0 || x > width || y < 0 || y > height || row < 0 || row > width || column < 0 || column > width-1){
-            throw out_of_range("Out of range values");
-        }else{
+    void setValue(int x,int y,int row,int column,float value,string formula,bool modified);
 
-            if(modified){
-                values.find(make_pair(row,column))->second.removeObservers();
-                values.find(make_pair(row,column))->second.removeSubjects();
-            }
-
-            values[make_pair(row,column)].setRow(row);
-            values[make_pair(row,column)].setColumn(column);
-            values[make_pair(row,column)].setValue(value);
-            values[make_pair(row,column)].setFormula(formula);
-            notify(x,y,value);
-        }
-    }
-
-    void setValue(int x,int y,int row,int column,string value,string formula,bool modified) {
-        if(x < 0 || x > width || y < 0 || y > height || row < 0 || row > width || column < 0 || column > width-1){
-            throw out_of_range("Out of range values");
-        }else{
-            if(modified){
-                values.find(make_pair(row,column))->second.removeObservers();
-                values.find(make_pair(row,column))->second.removeSubjects();
-            }
-
-            values[make_pair(row,column)].setRow(row);
-            values[make_pair(row,column)].setColumn(column);
-            values[make_pair(row,column)].setValue(0);
-            values[make_pair(row,column)].setFormula(formula);
-            notify(x,y,value);
-        }
-    }
+    void setValue(int x,int y,int row,int column,string value,string formula,bool modified);
 
     int getWidth() const;
 
